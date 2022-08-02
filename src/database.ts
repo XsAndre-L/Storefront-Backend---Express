3d3ae4e70023e41f046001442ff94f116950edf2
XsAndre-L <ahloubser12@gmail.com>
2022-08-02 14:48:45 +0200

    env

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,8 @@
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import { Pool, QueryResult } from "pg";
 
-// dotenv.config();
+dotenv.config();
 
 const {
     POSTGRES_HOST,
