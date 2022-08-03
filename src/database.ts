f7bfbdd979dc9c0d87a23642d48fe665d0ecfea8
XsAndre-L <ahloubser12@gmail.com>
2022-08-03 10:40:29 +0200

    'auto'

--- src/database.ts
+++ src/database.ts
@@ -4,25 +4,107 @@ import jwt from "jsonwebtoken";
 
 import { Pool, QueryResult } from "pg";
 
+// export class Database {
+//     envOBJ: any = {};
+//     database: Pool | undefined;
+
+//     constructor() {
+//         database = this.setDatabaseVals();
+//         dotenv.config();
+//     }
+
+//     getEnv() {
+//         new Promise((resolve) => {
+//             setTimeout(() => {
+//                 exec(
+//                     "/opt/elasticbeanstalk/bin/get-config --output JSON environment",
+//                     (error, stdout, stderr) => {
+//                         console.log("--- CORRECT DATA ---");
+//                         console.log(error, stdout, stderr);
+//                         this.envOBJ = stdout;
+//                     }
+//                 );
+//             }, 3000);
+//         }).then(
+//             (_) =>
+//                 new Promise((resolve) => {
+//                     console.log("then");
+//                 })
+//         );
+//     }
+
+//     // const {
+//     //     POSTGRES_HOST,
+//     //     POSTGRES_PORT,
+//     //     POSTGRES_DB,
+//     //     POSTGRES_DB_TEST,
+//     //     POSTGRES_USER,
+//     //     POSTGRES_PASSWORD,
+
+//     //     ENV,
+
+//     //     JWT_SIGN_TOKEN,
+//     // } = process.env;
+//     setDatabaseVals(): Pool {
+//         console.log(this.envOBJ.POSTGRES_HOST);
+//         if (this.envOBJ.ENV === "dev") {
+//             return new Pool({
+//                 host: this.envOBJ.POSTGRES_HOST,
+//                 port: parseInt(String(this.envOBJ.POSTGRES_PORT)),
+//                 database: this.envOBJ.POSTGRES_DB,
+//                 user: this.envOBJ.POSTGRES_USER,
+//                 password: this.envOBJ.POSTGRES_PASSWORD,
+//             });
+//         } else {
+//             return new Pool({
+//                 host: this.envOBJ.POSTGRES_HOST,
+//                 port: parseInt(String(this.envOBJ.POSTGRES_PORT)),
+//                 database: this.envOBJ.POSTGRES_DB_TEST,
+//                 user: this.envOBJ.POSTGRES_USER,
+//                 password: this.envOBJ.POSTGRES_PASSWORD,
+//             });
+//         }
+//     }
+
+//     // FUNCTION TO CONNECT, QUERY AND DISCONNECT DATABASE
+//     dbConnection = async (
+//         sql: string,
+//         sqlInput: any // the use of any is required in this case to be able to handle any data type.
+//     ): Promise<QueryResult<any>> => {
+//         // QueryResult<any> is the type returned by the database
+//         const conn = await database.connect();
+//         const result = await conn.query(sql, sqlInput);
+//         conn.release();
+
+//         return result;
+//     };
+
+//     // Input jwtToken
+//     // Returns user_id
+//     verifyUser = (auth: string | null): number => {
+//         try {
+//             const verification = jwt.verify(
+//                 String(auth),
+//                 String(this.envOBJ.JWT_SIGN_TOKEN)
+//             );
+//             return Object.values(verification)[0];
+//         } catch (error) {
+//             throw new Error(`Authentication Error`);
+//         }
+//     };
+// }
+
 dotenv.config();
 let envOBJ: any = {};
+let database: Pool;
 
-new Promise((resolve) => {
-    setTimeout(() => {
-        exec(
-            "/opt/elasticbeanstalk/bin/get-config --output JSON environment",
-            (error, stdout, stderr) => {
-                console.log("--- CORRECT DATA ---");
-                console.log(error, stdout, stderr);
-                envOBJ = stdout;
-            }
-        );
-    }, 3000);
-}).then(
-    (_) =>
-        new Promise((resolve) => {
-            console.log("then");
-        })
+exec(
+    "/opt/elasticbeanstalk/bin/get-config --output JSON environment",
+    (error, stdout, stderr) => {
+        console.log("--- CORRECT DATA ---");
+        console.log(error, stdout, stderr);
+        envOBJ = stdout;
+    }
 );
 
 // const {
@@ -38,7 +120,6 @@ new Promise((resolve) => {
 //     JWT_SIGN_TOKEN,
 // } = process.env;
 
-let database: Pool;
 console.log(envOBJ.POSTGRES_HOST);
 if (envOBJ.ENV === "dev") {
     database = new Pool({
